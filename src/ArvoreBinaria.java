90345289a90632e830c36aa658394e30f0e0df71
Pedro Pierre <pedro.pierre.jr@gmail.com>
2025-06-15 15:47:20 -0300

    aajuste na identação e inclusão dos métodos

--- src/ArvoreBinaria.java
+++ src/ArvoreBinaria.java
@@ -1,39 +1,35 @@
-
-
 public class ArvoreBinaria {
     private No raiz;
-    //construtor padrão
+
     public boolean estaVazia(){
         return raiz == null;
     }
+
     public void insere(Pessoa info){
         No novo = new No(info);
         if(estaVazia()){
             raiz = novo;
-        }
-        else{
+        } else {
             insereRec(novo, raiz);
         }
     }
+
     private void insereRec(No novo, No atual){
-        if(novo.getInfo() > atual.getInfo()){ //vai para a direita
+        if(novo.getInfo().getId() > atual.getInfo().getId()){ // direita
             if(atual.getDireita() == null){
                 atual.setDireita(novo);
-            }
-            else{
+            } else {
                 insereRec(novo, atual.getDireita());
             }
-        }
-        else{ //vai para a esquerda
+        } else { // esquerda
             if(atual.getEsquerda() == null){
                 atual.setEsquerda(novo);
-            }
-            else{
+            } else {
                 insereRec(novo, atual.getEsquerda());
             }
         }
     }
-    //usar o percurso em-ordem (e-r-d) para criar o toString
+
     @Override
     public String toString(){
         if(estaVazia()){
@@ -41,6 +37,7 @@ public class ArvoreBinaria {
         }
         return toStringRec(raiz);
     }
+
     private String toStringRec(No atual){
         if(atual == null) return "";
         String s = "";
@@ -49,50 +46,84 @@ public class ArvoreBinaria {
         s += toStringRec(atual.getDireita());
         return s;
     }
+
     public int altura(){
-        if(estaVazia() || raiz.getEsquerda() == null && raiz.getDireita() == null){
+        if(estaVazia() || (raiz.getEsquerda() == null && raiz.getDireita() == null)){
             return 0;
         }
         return alturaRec(raiz);
     }
+
     private int alturaRec(No atual){
-        if(atual== null || atual.getDireita() == null && atual.getEsquerda() == null){
+        if(atual == null || (atual.getDireita() == null && atual.getEsquerda() == null)){
             return 0;
         }
         int alturaEsq = alturaRec(atual.getEsquerda());
         int alturaDir = alturaRec(atual.getDireita());
         return alturaDir > alturaEsq ? alturaDir+1 : alturaEsq+1;
     }
+
     public int contaNos(){
-       if(estaVazia()){
+        if(estaVazia()){
             return 0;
         }
         return contaNosRec(raiz);
     }
+
     private int contaNosRec(No atual){
         if(atual == null) return 0;
         return contaNosRec(atual.getEsquerda()) + 1 + contaNosRec(atual.getDireita());
     }
-    public boolean  busca(int x){
+
+    public boolean busca(int x){
         if(estaVazia()){
             return false;
         }
         return buscaRec(x, raiz);
     }
+
     private boolean buscaRec (int x, No atual){
         if(atual == null){
             return false;
         }
-        if(atual.getInfo() == x) return true;
-        if(x >  atual.getInfo()) return buscaRec(x, atual.getDireita());
+        if(atual.getInfo().getId() == x) return true;
+        if(x > atual.getInfo().getId()) return buscaRec(x, atual.getDireita());
         return buscaRec(x, atual.getEsquerda());
     }
+
     public int maior(){
         if(estaVazia()) throw new RuntimeException("Arvore Vazia");
         No atual = raiz;
         while(atual.getDireita() != null){
             atual = atual.getDireita();
         }
-        return atual.getInfo();
+        return atual.getInfo().getId();
+    }
+
+    // extra para verificar se uma pessoa com esse nome já foi atendida
+    public boolean foiAtendida(String nome) {
+        return buscaNomeRec(nome, raiz);
+    }
+
+    private boolean buscaNomeRec(String nome, No atual){
+        if(atual == null) return false;
+        if(atual.getInfo().getNome().equalsIgnoreCase(nome)) return true;
+        return buscaNomeRec(nome, atual.getEsquerda()) || buscaNomeRec(nome, atual.getDireita());
+    }
+
+    public void imprimirEmOrdem() {
+        if (estaVazia()) {
+            System.out.println("Nenhum atendimento registrado.");
+        } else {
+            imprimirRec(raiz);
+        }
+    }
+
+    private void imprimirRec(No atual) {
+        if (atual != null) {
+            imprimirRec(atual.getEsquerda());
+            System.out.println(atual.getInfo());
+            imprimirRec(atual.getDireita());
+        }
     }
 }
